588889bebf252c74631b7890d7f36202127e2a01
TG <ebiritg@gmail.com>
2024-10-17 01:43:49 +0100

    first API

---
+++ controllers/AppController.js
@@ -0,0 +1,24 @@
+const { redisClient } = require('../utils/redis');
+const { dbClient } = require('../utils/db');
+
+class AppController {
+    static async getStatus(request, response) {
+        const redisAlive = redisClient.isAlive();
+        const dbAlive = dbClient.isAlive();
+        response.status(200).json({ redis: redisAlive, db: dbAlive })
+    }
+
+    static async getStats(request, response) {
+        try {
+            const userCount = await dbClient.nbUsers();
+            const filesCount = await dbClient.nbFiles();
+            response.status(200).json({ users: userCount, files: filesCount});
+
+        } catch(error){
+            console.error('Error fetching stats:', error);
+            response.status(500).json({error: "Unable to fetch stats"})
+        }
+    }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { dbClient } = require('./utils/db');
+const { redisClient } = require('./utils/redis');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
