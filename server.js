12d6b9062fa28c75646b8c2bbbd2e162af16b77e
TG <ebiritg@gmail.com>
2024-10-19 00:21:49 +0100

    add all

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+/**
+ * This is the main server file for the ALX Files Manager application.
+ * It sets up an Express server, defines routes, and starts the server.
+ */
+
 const express = require('express');
-const dbClient  = require('./utils/db');
-const  redisClient  = require('./utils/redis');
-const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const routes = require('./routes');
 
+const port = parseInt(process.env.PORT, Number) || 5000;
 
+app.use(express.json({ limit: '50mb' }));
 app.use('/', routes);
 
+/**
+ * Starts the server and listens on the specified port.
+ * @param {number} port - The port number to listen on.
+ */
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server running on ${port}`);
+});
